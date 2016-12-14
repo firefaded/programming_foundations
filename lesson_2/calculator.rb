904cc1bc4263189e081ced6ba01a6d3a9a5960e7
Kari Schendel <firefaded@yahoo.com>
2016-12-14 06:18:48 +0000

    ran calculator program through rubocop

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -1,6 +1,6 @@
 # Command Line Calculator
 
-#pseudocode for program
+# pseudocode for program
 # ask the user for two numbers
 # ask the user for an operation to perform
 # perform the operation on the two numbers
@@ -25,7 +25,8 @@ number2 = Kernel.gets().chomp()
 # Kernel.puts(number1.chomp().inspect())
 # Kernel.puts("the number is: " + number1 + "!")
 
-Kernel.puts("What operation would you like to perform? 1) add 2) subtract 3) multiply 4) divide")
+Kernel.puts("What operation would you like to perform? 1) add 2) subtract
+3) multiply 4) divide")
 operator = Kernel.gets().chomp()
 
 # checks to see what answer they chose
